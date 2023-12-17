85abeefec26997b08bc0f0514d8a984096fecde5
rn <mtsenkman@gmail.com>
2023-12-17 16:22:53 +0200

    commit

--- server.js
+++ server.js
@@ -1,103 +1,165 @@
 const express = require('express');
 const pool = require('./database');
 const cors = require('cors')
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors());
-
-//The express.json() function is a built-in middleware function in Express. 
-//It parses incoming requests with JSON payloads and is based on body-parser.
+// app.use(cors());
+app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
 app.use(express.json());
+app.use(cookieParser());
 
-//"async and await make promises easier to write"
-// async makes a function return a Promise
-// The keyword async before a function makes the function return a promise.
-// Syntax:  "async(req, res) => {}"
-// await makes a function wait for a Promise
-// The await keyword can only be used inside an async function.
-// The await keyword makes the function pause the execution and wait for a resolved promise before it continues
-// Syntax:  "async(req, res) => {let value = await promise}"
+const secret = "gdgdhdbcb770785rgdzqws";
+const maxAge = 60 * 60;
 
-/* 
-// Task 1
-app.post('/api/posts', async(req, res) => {
+const generateJWT = (id) => {
+    return jwt.sign({ id }, secret, { expiresIn: maxAge })
+}
+
+app.get('/auth/authenticate', async(req, res) => {
+    console.log('authentication request has been arrived');
+    const token = req.cookies.jwt; // assign the token named jwt to the token const
+    //console.log("token " + token);
+    let authenticated = false; // a user is not authenticated until proven the opposite
     try {
-        console.log("a post request has arrived");
-        const post = req.body;
-        const newpost = await pool.query(
-            "INSERT INTO posttable(title, body, urllink) values ($1, $2, $3)    RETURNING*", [post.title, post.body, post.urllink]
-            // $1, $2, $3 are mapped to the first, second and third element of the passed array (post.title, post.body, post.urllink)
-            // The RETURNING keyword in PostgreSQL allows returning a value from the insert or update statement.
-            // using "*" after the RETURNING keyword in PostgreSQL, will return everything
-        );
-        res.json(newpost);
+        if (token) {
+            await jwt.verify(token, secret, (err) => { //token exists, now we try to verify it
+                if (err) { // not verified, redirect to login page
+                    console.log(err.message);
+                    console.log('token is not verified');
+                    res.send({ "authenticated": authenticated }); // authenticated = false
+                } else { // token exists and it is verified
+                    console.log('author is authinticated');
+                    authenticated = true;
+                    res.send({ "authenticated": authenticated }); // authenticated = true
+                }
+            })
+        } else { //applies when the token does not exist
+            console.log('author is not authinticated');
+            res.send({ "authenticated": authenticated }); // authenticated = false
+        }
     } catch (err) {
         console.error(err.message);
+        res.status(400).send(err.message);
     }
-}); 
-*/
+});
 
-/* 
-// Task 2
-app.get('/api/posts', async(req, res) => {
+app.post('/auth/signup', async(req, res) => {
+    try {
+        console.log("a signup request has arrived");
+        const { email, password } = req.body;
+
+        const salt = await bcrypt.genSalt(); //  generates the salt, i.e., a random string
+        const bcryptPassword = await bcrypt.hash(password, salt) // hash the password and the salt
+        const authUser = await pool.query( // insert the user and the hashed password into the database
+            "INSERT INTO users(email, password) values ($1, $2) RETURNING*", [email, bcryptPassword]
+        );
+        console.log(authUser.rows[0].id);
+        const token = await generateJWT(authUser.rows[0].id); // generates a JWT by taking the user id as an input (payload)
+        res
+            .status(201)
+            .cookie('jwt', token, { maxAge: 6000000, httpOnly: true })
+            .json({ user_id: authUser.rows[0].id })
+            .send;
+    } catch (err) {
+        console.error(err.message);
+        res.status(400).send(err.message);
+    }
+});
+
+app.post('/auth/login', async(req, res) => {
+    try {
+        console.log("a login request has arrived");
+        const { email, password } = req.body;
+        const user = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+        if (user.rows.length === 0) return res.status(401).json({ error: "User is not registered" });
+
+        //Checking if the password is correct
+        const validPassword = await bcrypt.compare(password, user.rows[0].password);
+        //console.log("validPassword:" + validPassword);
+        if (!validPassword) return res.status(401).json({ error: "Incorrect password" });
+
+        const token = await generateJWT(user.rows[0].id);
+        res
+            .status(201)
+            .cookie('jwt', token, { maxAge: 6000000, httpOnly: true })
+            .json({ user_id: user.rows[0].id })
+            .send;
+    } catch (error) {
+        res.status(401).json({ error: error.message });
+    }
+});
+
+app.get('/auth/logout', (req, res) => {
+    console.log('delete jwt request arrived');
+    res.status(202).clearCookie('jwt').json({ "Msg": "cookie cleared" }).send
+});
+
+
+app.get('/posts', async(req, res) => {
     try {
         console.log("get posts request has arrived");
         const posts = await pool.query(
             "SELECT * FROM posttable"
         );
         res.json(posts.rows);
+        console.log(posts.rows)
     } catch (err) {
         console.error(err.message);
     }
 });
- */
 
-/* 
-// Task 3
-app.get('/api/posts/:id', async(req, res) => {
+
+app.post('/addPost', async(req, res) => {
     try {
-        console.log("get a post with route parameter  request has arrived");
-        // The req.params property is an object containing properties mapped to the named route "parameters". 
-        // For example, if you have the route /posts/:id, then the "id" property is available as req.params.id.
-        const { id } = req.params; // assigning all route "parameters" to the id "object"
-        const posts = await pool.query( // pool.query runs a single query on the database.
-            //$1 is mapped to the first element of { id } (which is just the value of id). 
+        console.log("a post request has arrived");
+        const post = req.body;
+        const newpost = await pool.query(
+            "INSERT INTO posttable(body, date) values ($1, $2)    RETURNING*", [post.body, post.date]
+        );
+        res.json(newpost);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+
+app.get('/posts/:id', async(req, res) => {
+    try {
+        console.log("get a post with route parameter request has arrived");
+        const { id } = req.params;
+        const posts = await pool.query(
             "SELECT * FROM posttable WHERE id = $1", [id]
         );
-        res.json(posts.rows[0]); // we already know that the row array contains a single element, and here we are trying to access it
-        // The res.json() function sends a JSON response. 
-        // This method sends a response (with the correct content-type) that is the parameter converted to a JSON string using the JSON.stringify() method.
+        res.json(posts.rows[0]);
     } catch (err) {
         console.error(err.message);
     }
 }); 
-*/
 
-/* 
-// Task 4
-app.put('/api/posts/:id', async(req, res) => {
+
+app.put('/posts/:id', async(req, res) => {
     try {
         const { id } = req.params;
         const post = req.body;
         console.log("update request has arrived");
         const updatepost = await pool.query(
-            "UPDATE posttable SET (title, body, urllink) = ($2, $3, $4) WHERE id = $1", [id, post.title, post.body, post.urllink]
+            "UPDATE posttable SET (body, date) = ($2, $3) WHERE id = $1", [id, post.body, post.date]
         );
         res.json(updatepost);
     } catch (err) {
         console.error(err.message);
     }
 });
- */
 
-/* 
-// Task 5
-app.delete('/api/posts/:id', async(req, res) => {
+
+app.delete('/posts/:id', async(req, res) => {
     try {
         const { id } = req.params;
-        //const post = req.body; // we do not need a body for a delete request
         console.log("delete a post request has arrived");
         const deletepost = await pool.query(
             "DELETE FROM posttable WHERE id = $1", [id]
@@ -106,8 +168,20 @@ app.delete('/api/posts/:id', async(req, res) => {
     } catch (err) {
         console.error(err.message);
     }
-}); 
-*/
+});
+
+app.delete('/posts/delete-all', async (req, res) => {
+    try {
+        console.log("Delete all posts request has arrived");
+        const deleteAllPosts = await pool.query(
+            "DELETE FROM posttable"
+        );
+        res.json(deleteAllPosts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
 
 app.listen(port, () => {
     console.log("Server is listening to port " + port)
